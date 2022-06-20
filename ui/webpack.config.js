51c31d98a63771b0c1f70f72dfb74f11bbe5c876
Mampionona33 <ramamps33@gmail.com>
2022-06-20 12:55:59 +0300

    finish to first setup webpack

---
+++ ui/webpack.config.js
@@ -0,0 +1,37 @@
+path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: { app: ['./src/index.js'] },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'all',
+    },
+  },
+};
