61a27b4cf01e8fd8f85df0b41030e5170f99e89a
Mampionona33 <ramamps33@gmail.com>
2022-06-21 12:26:29 +0300

    start to fix google auth

--- ui/src/pages/Login.jsx
+++ ui/src/pages/Login.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { GoogleLogin } from '@react-oauth/google';
+import jwt_decode from 'jwt-decode';
 
-export default function Login(params) {
+export default function Login() {
+  const handleOnSucces = (response) => {
+    const jwt = response.credential;
+    const userObject = jwt_decode(jwt);
+    console.log(userObject);
+  };
+
   return (
     <>
-      <GoogleLogin
-        onSuccess={(credentialResponse) => console.log(credentialResponse)}
-      />
+      <GoogleLogin onSuccess={(res) => handleOnSucces(res)} />
     </>
   );
 }
