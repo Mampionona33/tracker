6418d8c7a0528f4775ea530d0691e178c363c95d
Mampionona33 <ramamps33@gmail.com>
2022-06-21 21:00:02 +0300

    start to handle auth

--- ui/src/index.jsx
+++ ui/src/index.jsx
@@ -4,17 +4,29 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { UserProvider } from './auth/auth-context';
-import { ApolloClient, ApolloProvider, operationName } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+  operationName,
+} from '@apollo/client';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
-const client = new ApolloClient({
-  uri: process.env.UI_API_ENDPOINT,
-});
+const createApolloClient = () => {
+  const link = new HttpLink({
+    uri: process.env.UI_API_ENDPOINT,
+  });
+  return new ApolloClient({
+    link,
+    cache: new InMemoryCache(),
+  });
+};
 
 root.render(
-  <ApolloProvider client={client}>
+  <ApolloProvider client={createApolloClient()}>
     <UserProvider>
       <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
         <BrowserRouter>
