6f522ec64070c48d447547fc5e35981a08efb7d1
Mampionona33 <ramamps33@gmail.com>
2022-06-22 15:09:32 +0300

    cleaning code

--- ui/src/pages/Login.jsx
+++ ui/src/pages/Login.jsx
@@ -1,27 +1,11 @@
 import React, { useContext } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import { AuthContext } from '../context/authContext';
-import { gql } from '@apollo/client';
 import { useNavigate } from 'react-router-dom';
-
-const LOGIN_USER = gql`
-  mutation login($loginInput: LoginInput) {
-    loginUser(loginInput: $loginInput) {
-      token
-    }
-  }
-`;
 
 export default function Login(props) {
   const context = useContext(AuthContext);
   const navigate = useNavigate();
-
-  // const [loginUser, { loading }] = useMutation(LOGIN_USER, {
-  //   update(proxy, { data: userData }) {
-  //     context.login(userData);
-  //     navigate('/dashboard');
-  //   },
-  // });
 
   const handleOnSucces = (response) => {
     const jwt = response.credential;
