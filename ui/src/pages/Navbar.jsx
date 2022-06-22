06244d1ca708a077528f381751b8d5b924798fe0
Mampionona33 <ramamps33@gmail.com>
2022-06-22 15:58:41 +0300

    finish to add style

--- ui/src/pages/Navbar.jsx
+++ ui/src/pages/Navbar.jsx
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../context/authContext';
-
+import '../style/Navbar.scss';
 export default function Navbar(props) {
   const context = useContext(AuthContext);
 
   return (
-    <>
+    <div className='navbar'>
       <button onClick={() => context.logout()}>logout</button>
-    </>
+    </div>
   );
 }
--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -29,6 +29,21 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
     ],
   },
   optimization: {
