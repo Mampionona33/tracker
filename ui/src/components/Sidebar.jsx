0f7944e03a4593b62dad71455f6bf7db9f16d9bb
Mampionona33 <ramamps33@gmail.com>
2022-06-23 12:44:27 +0300

    Sidebar.jsx : change inline funciton onClick

--- ui/src/components/Sidebar.jsx
+++ ui/src/components/Sidebar.jsx
@@ -5,15 +5,26 @@ import '../style/Sidebar.scss';
 export default function Sidebar(props) {
   const navigate = useNavigate();
 
+  const handleClickItem = (event, target) => {
+    event.preventDefault();
+    navigate(target);
+  };
+
   return (
     <div className='sidebar'>
-      <div className='sidebar__element' onClick={() => navigate('/dashboard')}>
+      <div
+        className='sidebar__element'
+        onClick={(ev) => handleClickItem(ev, '/dashboard')}
+      >
         <span className='material-icons-round'>grid_view</span>
         <Link className='sidebar__element__link' to={'/dashboard'}>
           Dashboard
         </Link>
       </div>
-      <div className='sidebar__element' onClick={() => navigate('/history')}>
+      <div
+        className='sidebar__element'
+        onClick={(ev) => handleClickItem(ev, '/history')}
+      >
         <span className='material-icons-round'>work_history</span>
         <Link className='sidebar__element__link' to={'/history'}>
           History
