f97372611301a9233d4ddd6563c9e5ab0abb7b2e
Mampionona33 <ramamps33@gmail.com>
2022-06-23 12:39:18 +0300

    create Card.jsx and Card.scss

---
+++ ui/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/Card.scss';
+const Card = () => {
+  return (
+    <div className='card'>
+      <h2 className='card__title'>Title</h2>
+      <hr className='card__hr' />
+      <div className='card__body'>
+        fdsqfqsdfsqf dfsqdfdsqdf dsfsqdfsqfsfd sqdfqsfdqsf
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- ui/src/pages/Dashboard.jsx
+++ ui/src/pages/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Card from '../components/Card';
+import '../style/Dashboard.scss';
 
 export default function Dashboard() {
   return (
-    <>
-      <div className='dashboard'>placeholder for dashboard</div>
-    </>
+    <div className='dashboard'>
+      <Card />
+      <Card />
+    </div>
   );
 }
