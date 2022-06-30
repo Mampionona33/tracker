5dcf3a8eca6c24a170d44ded6523ebf66cf57c19
Mampionona33 <ramamps33@gmail.com>
2022-06-30 10:03:18 +0300

    stylized DialogNewTask.jsx

--- ui/src/components/DialogNewTask.jsx
+++ ui/src/components/DialogNewTask.jsx
@@ -20,7 +20,54 @@ const DialogNewTask = () => {
     <div>
       <div className='dialogNewTask'>
         <div className='dialogNewTask__container'>
-          <p>placeholder for dialog new task</p>
+          <form action='' className='dialogNewTask__form'>
+            <div className='dialogNewTask__form__el'>
+              <label htmlFor='boothNumber'>BOOTH NUMBER</label>
+              <input type='text' id='boothNumber' name='boothNumber' />
+            </div>
+
+            <div className='dialogNewTask__form__el'>
+              <label htmlFor='taskType'>TASK TYPE</label>
+              <select name='taskType' id='taskType'>
+                <option value={'Contenu'}>Contenu</option>
+              </select>
+            </div>
+
+            <div className='dialogNewTask__form__el'>
+              <label htmlFor='url'>URL</label>
+              <input type='text' id='url' name='url' />
+            </div>
+
+            <div className='dialogNewTask__form__el'>
+              <label htmlFor='cat'>CAT</label>
+              <input type='text' id='cat' name='cat' />
+            </div>
+
+            <div className='dialogNewTask__form__el'>
+              <label htmlFor='ivpn'>STATUS IVPN</label>
+              <select name='ivpn' id='ivpn'>
+                <option value={'I'}>I</option>
+                <option value={'V'}>V</option>
+                <option value={'P'}>P</option>
+                <option value={'N'}>N</option>
+              </select>
+            </div>
+
+            <div className='dialogNewTask__form__el'>
+              <label htmlFor='nbBefore'>NB BEFORE</label>
+              <input type='text' id='nbBefore' name='nbBefore' />
+            </div>
+
+            <div className='dialogNewTask__form__el'>
+              <label htmlFor='nbAfter'>NB AFTER</label>
+              <input type='text' id='nbAfter' name='nbAfter' />
+            </div>
+
+            <div className='dialogNewTask__form__el  comment'>
+              <label htmlFor='comment'>COMMENT</label>
+              <textarea type='text' id='comment' name='comment' />
+            </div>
+          </form>
           <div className='dialogNewTask__button'>
             <div
               className='dialogNewTask__button__save saveButton'
