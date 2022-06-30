172994f558f3ba634dc6c55158f34480e728fd0e
Mampionona33 <ramamps33@gmail.com>
2022-06-30 12:45:48 +0300

    DialogNewTask.jsx : get the fomr on console when click save

--- ui/src/components/DialogNewTask.jsx
+++ ui/src/components/DialogNewTask.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { componentContext } from '../context/componentContext';
 import '../style/DialogNewTask.scss';
 import Modale from './Modale';
@@ -8,6 +8,7 @@ const DialogNewTask = () => {
 
   const handleClickSave = (evnt) => {
     evnt.preventDefault();
+    console.log(newTask);
     ComponentContext.toggleDialogCreateNewTask();
   };
 
@@ -16,36 +17,81 @@ const DialogNewTask = () => {
     ComponentContext.toggleDialogCreateNewTask();
   };
 
+  const [newTask, setNewTask] = useState({
+    boothNumber: '',
+    taskType: '',
+    url: '',
+    cat: '',
+    ivpn: 'I',
+    nbBefore: '',
+    nbAfter: '',
+    comment: '',
+  });
+
+  const handleInputChange = (ev) => {
+    ev.preventDefault();
+    setNewTask({ ...newTask, [ev.target.name]: ev.target.value });
+  };
+
   return (
     <div>
       <div className='dialogNewTask'>
         <div className='dialogNewTask__container'>
-          <form action='' className='dialogNewTask__form'>
+          <form onSubmit={handleClickSave} className='dialogNewTask__form'>
             <div className='dialogNewTask__form__el'>
               <label htmlFor='boothNumber'>BOOTH NUMBER</label>
-              <input type='text' id='boothNumber' name='boothNumber' />
+              <input
+                type='text'
+                id='boothNumber'
+                name='boothNumber'
+                value={newTask.boothNumber}
+                onChange={(ev) => handleInputChange(ev)}
+              />
             </div>
 
             <div className='dialogNewTask__form__el'>
               <label htmlFor='taskType'>TASK TYPE</label>
-              <select name='taskType' id='taskType'>
+              <select
+                name='taskType'
+                id='taskType'
+                value={newTask.taskType}
+                onChange={(ev) => handleInputChange(ev)}
+              >
                 <option value={'Contenu'}>Contenu</option>
+                <option value={'Maj'}>Maj</option>
               </select>
             </div>
 
             <div className='dialogNewTask__form__el'>
               <label htmlFor='url'>URL</label>
-              <input type='text' id='url' name='url' />
+              <input
+                type='url'
+                id='url'
+                name='url'
+                value={newTask.url}
+                onChange={(ev) => handleInputChange(ev)}
+              />
             </div>
 
             <div className='dialogNewTask__form__el'>
               <label htmlFor='cat'>CAT</label>
-              <input type='text' id='cat' name='cat' />
+              <input
+                type='text'
+                id='cat'
+                name='cat'
+                value={newTask.cat}
+                onChange={(ev) => handleInputChange(ev)}
+              />
             </div>
 
             <div className='dialogNewTask__form__el'>
               <label htmlFor='ivpn'>STATUS IVPN</label>
-              <select name='ivpn' id='ivpn'>
+              <select
+                name='ivpn'
+                id='ivpn'
+                value={newTask.ivpn}
+                onChange={(ev) => handleInputChange(ev)}
+              >
                 <option value={'I'}>I</option>
                 <option value={'V'}>V</option>
                 <option value={'P'}>P</option>
@@ -55,33 +101,54 @@ const DialogNewTask = () => {
 
             <div className='dialogNewTask__form__el'>
               <label htmlFor='nbBefore'>NB BEFORE</label>
-              <input type='text' id='nbBefore' name='nbBefore' />
+              <input
+                type='number'
+                pattern='[0-9]{0,5}'
+                id='nbBefore'
+                name='nbBefore'
+                value={newTask.numberBefore}
+                onInput={(ev) => handleInputChange(ev)}
+                onChange={(ev) => handleInputChange(ev)}
+              />
             </div>
 
             <div className='dialogNewTask__form__el'>
               <label htmlFor='nbAfter'>NB AFTER</label>
-              <input type='text' id='nbAfter' name='nbAfter' />
+              <input
+                type='number'
+                id='nbAfter'
+                name='nbAfter'
+                value={newTask.numberAfter}
+                onInput={(ev) => handleInputChange(ev)}
+                onChange={(ev) => handleInputChange(ev)}
+              />
             </div>
 
             <div className='dialogNewTask__form__el  comment'>
               <label htmlFor='comment'>COMMENT</label>
-              <textarea type='text' id='comment' name='comment' />
+              <textarea
+                type='text'
+                id='comment'
+                name='comment'
+                value={newTask.comment}
+                onChange={(ev) => handleInputChange(ev)}
+              />
+            </div>
+            <div className='dialogNewTask__button'>
+              <button
+                type='submit'
+                className='dialogNewTask__button__save saveButton'
+              >
+                SAVE
+              </button>
+              <div
+                className='dialogNewTask__button__cancel cancelButton'
+                onClick={(ev) => handleClickCancel(ev)}
+              >
+                CANCEL
+              </div>
             </div>
           </form>
-          <div className='dialogNewTask__button'>
-            <div
-              className='dialogNewTask__button__save saveButton'
-              onClick={(ev) => handleClickSave(ev)}
-            >
-              SAVE
-            </div>
-            <div
-              className='dialogNewTask__button__cancel cancelButton'
-              onClick={(ev) => handleClickCancel(ev)}
-            >
-              CANCEL
-            </div>
-          </div>
         </div>
       </div>
       <Modale />
