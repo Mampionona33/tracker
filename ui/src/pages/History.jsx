7a1d9e034ba719041334fc010d79031c71750f21
Mampionona33 <ramamps33@gmail.com>
2022-07-08 14:48:59 +0300

    crete table in history

--- ui/src/pages/History.jsx
+++ ui/src/pages/History.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from '../context/authContext';
 import { GET_TASK_BY_DATE } from '../graphql/Query';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-
+import '../style/History.scss';
 export default function History(props) {
   const userContext = useContext(AuthContext);
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -34,11 +34,36 @@ export default function History(props) {
   return (
     <>
       <div className='history'>
-        <DatePicker
-          selected={selectedDate}
-          onChange={(date) => setSelectedDate(date)}
-          onSelect={(date) => handleDateSelect(date)}
-        />
+        <div className='history__cont'>
+          <div className='date_piker_cont'>
+            <DatePicker
+              selected={selectedDate}
+              onChange={(date) => setSelectedDate(date)}
+              onSelect={(date) => handleDateSelect(date)}
+            />
+          </div>
+          <table className='history__cont__table'>
+            <thead className='history__cont__table__head'>
+              <tr className='history__cont__table__head__title'>
+                <th className='border__round__top__left'>BOOTH NUMBER</th>
+                <th className='border__round__top'>SESSION</th>
+                <th className='border__round__top__right'>ACTION</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className='rows'>
+                <td>test</td>
+                <td>test2</td>
+                <td>ACTION</td>
+              </tr>
+              <tr className='rows'>
+                <td>test</td>
+                <td>test2</td>
+                <td>ACTION</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
