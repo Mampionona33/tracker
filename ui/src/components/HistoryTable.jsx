7105d3cfa2ddd65a9ba0a863a2ad5bb5deed2672
Mampionona33 <ramamps33@gmail.com>
2022-07-14 12:27:15 +0300

    styling table

--- ui/src/components/HistoryTable.jsx
+++ ui/src/components/HistoryTable.jsx
@@ -34,8 +34,8 @@ export default function HistoryTable() {
         let stop = ``;
         let duration = null;
         let sessionId = 0;
-
         const boothNumber = item.boothNumber;
+
         for (let i = 0; i < item.session.length; i++) {
           const startTmp = item.session[i].sessionStart;
           sessionId = i;
@@ -57,18 +57,26 @@ export default function HistoryTable() {
               ) {
                 stop = `${stpDate.getHours()}:${stpDate.getMinutes()}:${stpDate.getSeconds()}`;
 
-                duration = `${
-                  stpDate.getHours() - strDate.getHours()
-                } heures, ${
-                  stpDate.getMinutes() > strDate.getMinutes()
-                    ? stpDate.getMinutes() - strDate.getMinutes()
-                    : 59 + stpDate.getMinutes() - strDate.getMinutes()
-                } minutes et
+                duration = `${(stpDate.getHours() - strDate.getHours())
+                  .toString()
+                  .padStart(2, '0')} hours, ${
+                  stpDate.getMinutes() >= strDate.getMinutes()
+                    ? (stpDate.getMinutes() - strDate.getMinutes())
+                        .toString()
+                        .padStart(2, '0')
+                    : (59 + stpDate.getMinutes() - strDate.getMinutes())
+                        .toString()
+                        .padStart(2, '0')
+                } minutes and
                 
                 ${
-                  stpDate.getSeconds() > strDate.getSeconds()
-                    ? stpDate.getSeconds() - strDate.getSeconds()
-                    : 60 + stpDate.getSeconds() - strDate.getSeconds()
+                  stpDate.getSeconds() >= strDate.getSeconds()
+                    ? (stpDate.getSeconds() - strDate.getSeconds())
+                        .toString()
+                        .padStart(2, '0')
+                    : (60 + stpDate.getSeconds() - strDate.getSeconds())
+                        .toString()
+                        .padStart(2, '0')
                 }`;
               }
 
@@ -81,21 +89,37 @@ export default function HistoryTable() {
                 stop = `${stpDate.getHours()}:${stpDate.getMinutes()}:${stpDate.getSeconds()} (${stpDate.getFullYear()}-${stpDate.getMonth()}-${stpDate.getDate()})`;
                 duration = `                ${
                   stpDate.getMonth() === strDate.getMonth() &&
-                  stpDate.getDate() - strDate.getDate() - 1
-                } jours,                ${
+                  (stpDate.getDate() - strDate.getDate() - 1)
+                    .toString()
+                    .padStart(2, '0')
+                } days,                ${
                   stpDate.getHours() > strDate.getHours()
-                    ? stpDate.getHours() - strDate.getHours()
-                    : 24 + stpDate.getHours() - strDate.getHours()
-                } heures, ${
+                    ? (stpDate.getHours() - strDate.getHours())
+                        .toString()
+                        .padStart(2, '0')
+                    : (24 + stpDate.getHours() - strDate.getHours())
+                        .toString()
+                        .padStart(2, '0')
+                } hours, ${
                   stpDate.getMinutes() > strDate.getMinutes()
-                    ? stpDate.getMinutes() - strDate.getMinutes()
-                    : 59 + stpDate.getMinutes() - strDate.getMinutes()
-                } minutes et               
+                    ? (stpDate.getMinutes() - strDate.getMinutes())
+                        .toString()
+                        .padStart(2, '0')
+                    : (59 + stpDate.getMinutes() - strDate.getMinutes())
+                        .toString()
+                        .padStart(2, '0')
+                } minutes and               
                 ${
                   stpDate.getSeconds() > strDate.getSeconds()
-                    ? stpDate.getSeconds() - strDate.getSeconds()
-                    : 60 + stpDate.getSeconds() - strDate.getSeconds()
-                }`;
+                    ? (stpDate.getSeconds() - strDate.getSeconds())
+                        .toString()
+                        .padStart(2, '0')
+                    : (
+                        60 +
+                        stpDate.getSeconds() -
+                        strDate.getSeconds()
+                      ).toString(2, '0')
+                } secondes`;
               }
             }
 
--- ui/src/components/Table.jsx
+++ ui/src/components/Table.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTable, usePagination } from 'react-table';
+import '../style/Table.scss';
 
 function Table({ columns, data }) {
   const {
@@ -57,59 +58,62 @@ function Table({ columns, data }) {
               })}
             </tbody>
           </table>
-
-          <div className='pagination'>
-            <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-              {'<<'}
-            </button>{' '}
-            <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-              {'<'}
-            </button>{' '}
-            <button onClick={() => nextPage()} disabled={!canNextPage}>
-              {'>'}
-            </button>{' '}
-            <button
-              onClick={() => gotoPage(pageCount - 1)}
-              disabled={!canNextPage}
-            >
-              {'>>'}
-            </button>{' '}
-            <span>
-              Page{' '}
-              <strong>
-                {pageIndex + 1} of {pageOptions.length}
-              </strong>{' '}
-            </span>
-            <span>
-              | Go to page:{' '}
-              <input
-                type='number'
-                defaultValue={pageIndex + 1}
-                onChange={(e) => {
-                  const page = e.target.value ? Number(e.target.value) - 1 : 0;
-                  gotoPage(page);
-                }}
-                style={{ width: '100px' }}
-              />
-            </span>{' '}
-            <select
-              value={pageSize}
-              onChange={(e) => {
-                setPageSize(Number(e.target.value));
-              }}
-            >
-              {[3, 7, 15].map((pageSize) => (
-                <option key={pageSize} value={pageSize}>
-                  Show {pageSize}
-                </option>
-              ))}
-            </select>
-          </div>
         </div>
+      </div>
+      <div className='card pagination'>
+        <div className='directNav'>
+          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+            {'<<'}
+          </button>
+          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+            {'<'}
+          </button>
+          <button onClick={() => nextPage()} disabled={!canNextPage}>
+            {'>'}
+          </button>
+          <button
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          >
+            {'>>'}
+          </button>
+        </div>
+        <div className='pageControl'>
+          <span>
+            Page{' '}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{' '}
+          </span>
+          <span>
+            | Go to page:{' '}
+            <input
+              type='number'
+              defaultValue={pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                gotoPage(page);
+              }}
+              style={{ width: '100px' }}
+            />
+          </span>
+        </div>
+        <select
+          title='rowPerPage'
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[3, 7, 15].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
       </div>
     </>
   );
 }
-
 
 export default Table;
