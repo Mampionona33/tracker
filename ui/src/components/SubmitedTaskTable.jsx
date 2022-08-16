a79fd087b4358991f4a4ded2d261283f0e5f59f4
Mampionona33 <ramamps33@gmail.com>
2022-08-16 20:46:31 +0300

    SubmitedTaskState.jsx : getting data with isDone state

--- ui/src/components/SubmitedTaskTable.jsx
+++ ui/src/components/SubmitedTaskTable.jsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import { useQuery } from '@apollo/client';
+import React, { useContext, useEffect } from 'react';
+import { AuthContext } from '../context/authContext';
+import { GET_USER_TASK } from '../graphql/Query';
 
 export default function SubmitedTaskTable() {
+  const userContext = useContext(AuthContext);
+  const {
+    data: userTask,
+    error: errorFetchingUserTask,
+    loading: loadingUserTask,
+  } = useQuery(GET_USER_TASK, {
+    variables: {
+      input: {
+        sub: userContext.user.sub,
+      },
+    },
+  });
+
+  useEffect(() => {
+    if (userTask && userTask.getUserTask) {
+      const taskDoneList = Array.from(userTask.getUserTask).filter(
+        (item) => item.taskState === 'isDone'
+      );
+      taskDoneList && taskDoneList.length > 0 && console.log(taskDoneList);
+    }
+  }, [userTask]);
+
   return (
     <div style={{ height: 400, width: '100%' }}>
       <p>place holder sumbit task table</p>
