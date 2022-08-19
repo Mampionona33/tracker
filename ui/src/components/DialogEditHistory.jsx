f1880271e8098c62fd71df4a70469a77d10b98a4
Mampionona33 <ramamps33@gmail.com>
2022-08-19 21:19:10 +0300

    DialogHistory.jsx : format dialog

--- ui/src/components/DialogEditHistory.jsx
+++ ui/src/components/DialogEditHistory.jsx
@@ -5,16 +5,17 @@ import { HistoryContext } from '../context/historyContext';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import DialogTitle from './DialogTitle';
+import '../style/DialogEditHistory.scss';
+import { componentContext } from '../context/componentContext';
 
 function DialogEditHistory() {
   const historyContext = useContext(HistoryContext);
+  const ComponentContext = useContext(componentContext);
   const [selectedDate, setSelectedDate] = useState(
     historyContext.historyData.sessionstart
       ? new Date(historyContext.historyData.sessionstart)
       : new Date()
   );
-  // console.log(historyContext.historyData.sessionstart);
-  // console.log(new Date(historyContext.historyData.sessionstart));
 
   const handleDateSelect = (date) => {
     const select = `${new Date(date).getFullYear()}/${
@@ -28,18 +29,25 @@ function DialogEditHistory() {
     }
   };
 
+  const handleClickCancel = (event) => {
+    event.preventDefault();
+    ComponentContext.closeDialogEditHistory();
+  };
+
+  const handleClickSave = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+  };
+
   return (
     <>
       <div className='dialogEditHistory'>
         <div className='dialogEditHistory__formContainer'>
           <DialogTitle>EDIT HISTORY</DialogTitle>
-          <form>
-            <fieldset>
+          <form onSubmit={handleClickSave}>
+            <fieldset className='dialogEditHistory__fieldset'>
               <legend>START INFO</legend>
-              <div
-                className='dialogEditHistory__row'
-                style={{ display: 'flex', alignItems: 'center' }}
-              >
+              <div className='dialogEditHistory__fieldset__row'>
                 <label htmlFor='' style={{ display: 'inline' }}>
                   DATE
                 </label>
@@ -50,42 +58,50 @@ function DialogEditHistory() {
                 />
               </div>
 
-              <div>
+              <div className='dialogEditHistory__fieldset__row'>
                 <label htmlFor=''>HRS</label>
                 <input type='number' name='' id='' />
               </div>
 
-              <div>
+              <div className='dialogEditHistory__fieldset__row'>
                 <label htmlFor=''>MIN</label>
                 <input type='number' name='' id='' />
               </div>
 
-              <div>
+              <div className='dialogEditHistory__fieldset__row'>
                 <label htmlFor=''>SEC</label>
                 <input type='number' name='' id='' />
               </div>
             </fieldset>
-            <fieldset>
+            <fieldset className='dialogEditHistory__fieldset'>
               <legend>END INFO</legend>
-              <div>
+              <div className='dialogEditHistory__fieldset__row'>
                 <label htmlFor=''>HRS</label>
                 <input type='number' name='' id='' />
               </div>
 
-              <div>
+              <div className='dialogEditHistory__fieldset__row'>
                 <label htmlFor=''>MIN</label>
                 <input type='number' name='' id='' />
               </div>
 
-              <div>
+              <div className='dialogEditHistory__fieldset__row'>
                 <label htmlFor=''>SEC</label>
                 <input type='number' name='' id='' />
               </div>
             </fieldset>
             <hr />
-            <div className='buttonGroup'>
-              <button>SAVE</button>
-              <button>CANCEL</button>
+            <div className='dialogEditHistory__buttonGroup'>
+              <button type='submit' className='saveButton'>
+                SAVE
+              </button>
+              <button
+                type='reset'
+                onClick={handleClickCancel}
+                className='cancelButton'
+              >
+                CANCEL
+              </button>
             </div>
           </form>
         </div>
