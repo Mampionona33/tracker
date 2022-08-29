032f937935c4957b48d91ead9d21ca2323a46739
Mampionona33 <ramamps33@gmail.com>
2022-08-29 22:52:17 +0300

    installing terser-webpak-plugin

--- front/webpack.prod.js
+++ front/webpack.prod.js
@@ -1,6 +1,18 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        runtimeChunk: true,
+        removeAvailableModules: false,
+        removeEmptyChunks: false,
+        splitChunks: false,
+        pathinfo: false,
+      }),
+    ],
+  },
 });
