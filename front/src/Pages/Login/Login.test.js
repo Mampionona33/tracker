3cc422d30262508f45c46c8733886442fe247c81
Mampionona33 <ramamps33@gmail.com>
2022-09-02 10:35:15 +0300

    modify Login.test.js

--- front/src/Pages/Login/Login.test.js
+++ front/src/Pages/Login/Login.test.js
@@ -1,13 +1,20 @@
 import { render, screen } from '@testing-library/react';
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Login from './Login';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { expect } from '@jest/globals';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import { AuthConext } from '../../context/authContext';
 
 test('should render greeting', () => {
   render(
-    <GoogleOAuthProvider>
-      <Login />
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </GoogleOAuthProvider>
   );
   const greeting = screen.getByText('Welcome to mampionona task tracker');
