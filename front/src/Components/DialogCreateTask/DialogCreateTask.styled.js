da9198b8c844fcba3c816a96b37d16dce25edbbc
Mampionona33 <ramamps33@gmail.com>
2022-09-09 11:07:37 +0300

    stylize DialogCreateTask.styled.js

--- front/src/Components/DialogCreateTask/DialogCreateTask.styled.js
+++ front/src/Components/DialogCreateTask/DialogCreateTask.styled.js
@@ -29,16 +29,29 @@ export const DialogCreateTaskFormLabel = styled.label`
 
 export const DialogCreateTaskInput = styled.input`
   margin: 0;
+  outline: none;
+  border-radius: 5px;
+  border: 1px solid gray;
+  :hover {
+    box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px,
+      rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+  }
+  :focus {
+    box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px,
+      rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+  }
 `;
 
 export const DialogCreateTaskSelect = styled.select`
   text-transform: uppercase;
+  border-radius: 5px;
+  outline: none;
 `;
 
 export const DialogCreateTaskOption = styled.option`
-  text-transform: uppercase;
   display: flex;
   justify-content: flex-end;
+  text-transform: uppercase;
 `;
 
 export const DialogCreateTaskTextarea = styled.textarea`
@@ -48,4 +61,6 @@ export const DialogCreateTaskTextarea = styled.textarea`
   border-radius: 0.2rem;
   max-height: 5rem;
   max-width: 30rem;
+  border-radius: 5px;
+  outline: none;
 `;
