8026015e02ec3be7e330be24467188a72c17f81f
Mampionona33 <ramamps33@gmail.com>
2022-10-04 08:22:37 +0300

    change init.mongo-.js

--- api/init.mongo-.js
+++ api/init.mongo-.js
@@ -279,7 +279,7 @@ const resetMongo = async () => {
       },
       {
         name: 'CREADemo',
-        goal: 60,
+        goal: 6.5,
         id: 32,
       },
       {
@@ -295,7 +295,7 @@ const resetMongo = async () => {
       {
         id: 35,
         name: 'AUTOVALIDCREADemo',
-        goal: 60,
+        goal: 6.5,
       },
       {
         id: 36,
@@ -366,17 +366,17 @@ const resetMongo = async () => {
     // initializers--------------------------
     db.collection('typeTask').deleteMany({});
     // Remove  collection and insert initializers
-    await collectionCounter.deleteMany({});
-    await collectionCounter.insertMany(initialiCounter);
+    // await collectionCounter.deleteMany({});
+    // await collectionCounter.insertMany(initialiCounter);
     const resultCounter = await collectionCounter.find({}).toArray();
 
-    await collectionUsers.deleteMany({});
-    await collectionUsers.insertMany(initialUser);
+    // await collectionUsers.deleteMany({});
+    // await collectionUsers.insertMany(initialUser);
     const resultUser = await collectionUsers.find({}).toArray();
 
-    await collectionTask.deleteMany({});
-    await collectionTask.insertMany(initTasks);
-    const resultTask = await collectionTask.find({}).toArray();
+    // await collectionTask.deleteMany({});
+    // await collectionTask.insertMany(initTasks);
+    // const resultTask = await collectionTask.find({}).toArray();
 
     await collectionTypeTask.deleteMany({});
     await collectionTypeTask.insertMany(initTaskType);
@@ -385,13 +385,13 @@ const resetMongo = async () => {
     // show result in terminal
     console.log(
       'Result of instert : \n',
-      resultCounter,
+      // resultCounter,
       '\n',
       '-----------\n',
-      resultUser,
+      // resultUser,
       '\n',
       '-----------\n',
-      resultTask,
+      // resultTask,
       '-----------\n',
       resultInitTaskType
     );
