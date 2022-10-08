e9aa29f365e7a0cb0bf8d408785b75346ff1928f
Mampionona33 <ramamps33@gmail.com>
2022-10-08 08:43:46 +0300

    PendingTaskPlayBtn.jsx : get processing task

--- front/src/Components/PendingTaskPlayBtn/PendingTaskPlayBtn.jsx
+++ front/src/Components/PendingTaskPlayBtn/PendingTaskPlayBtn.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import CircularBtn from '../CircularBtn/CircularBtn';
+import useGetProcessingTask from './../../assets/Hooks/useGetProcessingTask';
 
 function PendingTaskPlayBtn({ taskId }) {
+  const { processingTask } = useGetProcessingTask();
   const handleClick = (event) => {
     console.log('taskId', taskId);
+    console.log(
+      processingTask
+        ? processingTask.reduce((a, b) => a + b).id
+        : 'no processing task'
+    );
   };
   return (
     <div onClick={handleClick}>
