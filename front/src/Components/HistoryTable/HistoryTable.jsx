3d97d082a18b9bb3e691a0f7b2d3809fdb430e2a
Mampionona33 <ramamps33@gmail.com>
2022-10-11 14:43:26 +0300

    Get data to view in HitoryTable.jsx

--- front/src/Components/HistoryTable/HistoryTable.jsx
+++ front/src/Components/HistoryTable/HistoryTable.jsx
@@ -1,15 +1,17 @@
 import { useQuery } from '@apollo/client';
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import DataTable from 'react-data-table-component';
 import { AuthConext } from '../../context/authContext';
 import { GET_USER_TASK } from '../../Graphql/Query';
 import { HistoryTableCont } from './HistoryTable.styled';
 import moment from 'moment';
 import { useParams } from 'react-router-dom';
+import { difDate } from '../../assets/timeUtility';
 
 const HistoryTable = () => {
   const { sub } = useContext(AuthConext);
   const { date } = useParams();
+  const [columnData, setColumnData] = useState([]);
   const {
     data: userTasks,
     error: errorFetchUserTasks,
@@ -18,6 +20,7 @@ const HistoryTable = () => {
 
   useEffect(() => {
     if (userTasks && userTasks.getUserTask) {
+      const sessionTask = [];
       Array.from(userTasks.getUserTask).map((item) => {
         const boothNumber = item.boothNumber;
         Array.from(item.session).map((el) => {
@@ -26,28 +29,44 @@ const HistoryTable = () => {
           const selectedDate = moment(el.sessionStart).format('DD-MM-YYYY');
 
           if (selectedDate === date) {
-            console.log(
-              boothNumber,
-              'start : ',
-              moment(el.sessionStart).format('HH:mm:ss'),
-              'stop :',
-              moment(el.sessionStop).format('HH:mm:ss')
-            );
+            console.log(boothNumber);
+
+            console.log('dif date', difDate(el.sessionStart, el.sessionStop));
+
+            sessionTask.push({
+              boothNumber: boothNumber,
+              sessionStart: moment(el.sessionStart).format('HH:mm:ss'),
+              sessionStop: moment(el.sessionStop).format('HH:mm:ss'),
+            });
+            // console.log(
+            //   boothNumber,
+            //   'start : ',
+            //   moment(el.sessionStart).format('HH:mm:ss'),
+            //   'stop :',
+            //   moment(el.sessionStop).format('HH:mm:ss')
+            // );
           }
         });
       });
+
+      if (sessionTask.length > 0) {
+        setColumnData(sessionTask);
+      }
     }
   }, [userTasks]);
 
   const column = [
     {
       name: 'BOOTH NUMBER',
+      selector: (row) => row.boothNumber,
     },
     {
       name: 'START',
+      selector: (row) => row.sessionStart,
     },
     {
       name: 'STOP',
+      selector: (row) => row.sessionStop,
     },
     {
       name: 'DURATION',
@@ -59,7 +78,7 @@ const HistoryTable = () => {
 
   return (
     <HistoryTableCont>
-      <DataTable columns={column} />
+      <DataTable columns={column} data={columnData} />
     </HistoryTableCont>
   );
 };
